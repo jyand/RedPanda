e26a68ade3821a5165882ef6be1c774e997f5ea2
jyand <jyand@protonmail.com>
2020-04-14 03:12:25 +0000

    need to bundle w/ executables

--- defaults/pref/local-settings.js
+++ defaults/pref/local-settings.js
@@ -1,6 +1,3 @@
-// Intika Privafox MoD
 pref("general.config.filename", "BROWSER.js");
 
-// Default Firefox
-// ROT13 Settings primitive encryption routing that
 pref("general.config.obscure_value", 0);
